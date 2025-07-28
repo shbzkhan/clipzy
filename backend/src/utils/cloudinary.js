910ba0ec0181aad090d0c55e4333637fbc50431c
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-07-28 23:39:19 +0530

    fix cloudinary config

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({
-  cloud_name: "my_cloud_name",
-  api_key: "my_key",
-  api_secret: "my_secret",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 //upload image or video on cloudinary
 const uploadToCloudinary = async (localPath) => {
