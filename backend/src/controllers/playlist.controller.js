4a9c49dadc3b84acd1d2f1ee13c1561764164e72
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-08-05 11:06:06 -0700

    write delete playlist controller

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -154,7 +154,31 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    // TODO: delete playlist
+    if(!playlistId){
+        throw new apiError(404, "Playlist Id required")
+    }
+    if(!isValidObjectId(playlistId)){
+        throw new apiError(404, "Invalid Playlist Id")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+        if(!playlist){
+            throw new apiError(404, "Playlist not found")
+        }
+
+    if(playlist.owner.toString() !== req.user._id.toString()){
+        throw new apiError(401, "Only playlist owner can delete playlist")
+    }
+
+    const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId)
+    if(!deletedPlaylist){
+        throw new apiError("Playlist delete successfully")
+    }
+
+    return res
+            .status(200)
+            .json(new apiResponse(200, deletePlaylist, "Video remove from playlist"))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
