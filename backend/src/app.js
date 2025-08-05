edd21b08bb5c9495eaede56cb64851691361ae3f
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-08-05 10:21:05 -0700

    write create playlist controller and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,13 +21,15 @@ import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js";
+import playlistRouter from "./routes/playlist.routes.js"
 
 //router declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 
 
