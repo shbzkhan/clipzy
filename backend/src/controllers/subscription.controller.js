5672daf9dcedbe8d0e93a393ba0e0915da18aa33
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-10-13 21:17:14 +0530

    fix impoer sintex

--- backend/src/controllers/subscription.controller.js
+++ backend/src/controllers/subscription.controller.js
@@ -1,10 +1,10 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
-import { ApiError } from "../utils/ApiError.js
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { sendNotificationToDevice } from "../utils/sendNotificationToDevice.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   console.count("toggleSubscription called");
